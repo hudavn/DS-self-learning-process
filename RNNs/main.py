c3928e28e28e41c786fecc2c36a531e0c7971fef
dd-pc <ddforwork08@gmail.com>
2022-02-22 12:44:43 +0700

    Update model

--- RNNs/main.py
+++ RNNs/main.py
@@ -4,14 +4,107 @@
 # with open("wonderland.txt", "w") as f:
 #     f.write(data.text)
 
+# %%
 import numpy 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-import os
 
 filename = './wonderland.txt'
 raw_text = open(filename).read().lower()
-print(raw_text)
+
+#%%
+import string 
+
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+print('number of letters: ', len(char_to_int))
+# print(char_to_int)
+
+#%%
+chars_new = list(string.ascii_lowercase) + ['0', '.', ',', ' ', '!', '?', 'unk']
+chars_to_int = dict((v, k) for k, v in enumerate(chars_new))
+int_to_chars = dict((k, v) for k, v in enumerate(chars_new))
+# print('character to int:', chars_to_int)
+# print('int to character:', int_to_chars)
+
+#%%
+n_chars = len(raw_text)
+n_vocab = len(chars_new)
+print('Total characters: ', n_chars)
+print('Total Vocab: ', n_vocab)
     
+
+# %%
+def _encode_sen(text):
+    text = text.lower()
+    sen_vec = []
+    for let in text:
+        if let in chars_new[:-1]:
+            idx = chars_to_int[let]
+        else:
+            idx = chars_to_int['unk']
+        sen_vec.append(idx)
+    return sen_vec
+
+x_test = _encode_sen('Alice is a wonderful story. #')
+print(x_test)
+
+# %%
+def _decode_sen(vec):
+    text = []
+    for i in vec:
+        let = int_to_chars[i]
+        text.append(let)
+    text = ''.join(text)
+    return text
+
+x_test = _encode_sen('Alice is a wonderful story. #')
+_decode_sen(x_test)
+
+# %%
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+
+for i in range(0, n_chars - seq_length, 1):
+    # Lấy ra 100 kí tự liền trước
+    seq_in = raw_text[i:i + seq_length]
+    # Lấy ra kí tự liền sau 100 kí tự đó
+    seq_out = raw_text[i + seq_length]
+    dataX.append(_encode_sen(seq_in))
+    dataY.append(_encode_sen(seq_out)[0])
+    n_patterns = len(dataX)
+    
+print("Total Patterns: ", n_patterns)
+
+# %%
+# reshape X to be [samples, time steps, features]
+X_train = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+# normalize
+X_train = X_train / float(n_vocab)
+# one hot encode the output variable
+y_train = np_utils.to_categorical(dataY)
+print('X [samples, time steps, features] shape: ', X_train.shape)
+print('Y shape: ', y_train.shape)
+
+# %%
+import seaborn as sn
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize = (10, 5))
+sn.countplot(np.array(dataY))
+plt.xticks(ticks = np.arange(33), labels = np.array(chars_new))
+
+# %%
+model = Sequential()
+model.add(LSTM(256, input_shape = (X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(dataY.shape[1], activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+model.summary()
+
+# %%
